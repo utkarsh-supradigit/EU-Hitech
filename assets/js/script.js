bc51e76112357f28cf32c9d42a6dff39759c8728
utkarsh-supradigit <168803333+utkarsh-supradigit@users.noreply.github.com>
2024-10-17 17:43:28 +0530

    current changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,273 +47,55 @@
 		$(".offcanvas-menu-info,.offcanvas-menu-overlay").removeClass("active");
 	});
 
+	function initializeSlider() {
+		if ($(window).width() > 768) {
+			if (!$('.recent-work-slider-one').hasClass('slick-initialized')) {
+				$(".recent-work-slider-one").slick({
+					infinite: true,
+					speed: 500,
+					slidesToShow: 4,
+					dots: false,
+					autoplay: false,
+					slidesToScroll: 1,
+					arrows: true,
+					prevArrow: '<i class="fas left icon fa-arrow-left"></i>',
+					nextArrow: '<i class="fas right icon fa-arrow-right"></i>',
+					gap: '24px',
+					responsive: [{
+							breakpoint: 992,
+							settings: {
+								slidesToShow: 2,
+							},
+						},
+						{
+							breakpoint: 768,
+							settings: {
+								slidesToShow: 2,
+							},
+						},
+					],
+				});
+			} else {
+				if ($('.recent-work-slider-one').hasClass('slick-initialized')) {
+					$('.recent-work-slider-one').slick('unslick')
+				}
+			}
+		}
+	}
 
-	// recent work one slider js
-	$(".recent-work-slider-one").slick({
-		infinite: true,
-		speed: 500,
-		slidesToShow: 4,
-		dots: false,
-		autoplay: false,
-		slidesToScroll: 1,
-		arrows: true,
-		prevArrow: '<i class="fas left icon fa-arrow-left"></i>',
-		nextArrow: '<i class="fas right icon fa-arrow-right"></i>',
-		gap: '24px',
-		responsive: [{
-				breakpoint: 992,
-				settings: {
-					slidesToShow: 2,
-				},
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 1,
-				},
-			},
-		],
-	});
+	// Initialize slider on page load
+	// initializeSlider();
 
-	// active employes area slider one js
-	$(".active-employes-slider").slick({
-		infinite: true,
-		speed: 500,
-		slidesToShow: 3,
-		dots: true,
-		autoplay: false,
-		slidesToScroll: 1,
-		arrows: false,
-		prevArrow: '<i class="fas left icon fa-arrow-left"></i>',
-		nextArrow: '<i class="fas right icon fa-arrow-right"></i>',
-		gap: '24px',
-		responsive: [{
-				breakpoint: 992,
-				settings: {
-					slidesToShow: 2,
-				},
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 1,
-				},
-			},
-		],
-	});
-
-	// active employes area slider two js
-	$(".active-employes-slider-two").slick({
-		infinite: true,
-		speed: 500,
-		slidesToShow: 3,
-		dots: true,
-		autoplay: false,
-		slidesToScroll: 1,
-		arrows: false,
-		prevArrow: '<i class="fas left icon fa-arrow-left"></i>',
-		nextArrow: '<i class="fas right icon fa-arrow-right"></i>',
-		gap: '24px',
-		responsive: [{
-				breakpoint: 992,
-				settings: {
-					slidesToShow: 3,
-				},
-			},
-			{
-				breakpoint: 576,
-				settings: {
-					slidesToShow: 1,
-				},
-			},
-		],
-	});
-
-
-	// news area one slider js
-	$(".news-area-one-slider").slick({
-		infinite: true,
-		speed: 500,
-		slidesToShow: 3,
-		dots: false,
-		autoplay: false,
-		slidesToScroll: 1,
-		prevArrow: '<i class="fas left icon fa-arrow-left"></i>',
-		nextArrow: '<i class="fas right icon fa-arrow-right"></i>',
-		gap: '24px',
-		responsive: [{
-				breakpoint: 992,
-				settings: {
-					slidesToShow: 2,
-				},
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 1,
-				},
-			},
-		],
-	});
-
-
-	// news area two slider js
-	$(".news-area-two-slider").slick({
-		infinite: true,
-		speed: 500,
-		slidesToShow: 3,
-		dots: true,
-		autoplay: false,
-		arrows: false,
-		slidesToScroll: 1,
-		prevArrow: '<i class="fas left icon fa-arrow-left"></i>',
-		nextArrow: '<i class="fas right icon fa-arrow-right"></i>',
-		gap: '24px',
-		responsive: [{
-				breakpoint: 992,
-				settings: {
-					slidesToShow: 2,
-				},
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 1,
-				},
-			},
-			{
-				breakpoint: 576,
-				settings: {
-					slidesToShow: 1,
-					dots: false
-				},
-			},
-		],
-	});
-
-	// testimonial area one slider js
-	$(".testimonial-area-slider-one").slick({
-		infinite: true,
-		speed: 500,
-		slidesToShow: 2,
-		dots: true,
-		autoplay: false,
-		arrows: false,
-		slidesToScroll: 1,
-		prevArrow: '<i class="fas left icon fa-arrow-left"></i>',
-		nextArrow: '<i class="fas right icon fa-arrow-right"></i>',
-		gap: '24px',
-		responsive: [{
-				breakpoint: 992,
-				settings: {
-					slidesToShow: 1,
-				},
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 1,
-				},
-			},
-		],
-	});
-
-
-	// testimonial area two slider js
-	$(".testimonial-slider-two").slick({
-		infinite: true,
-		speed: 500,
-		slidesToShow: 2,
-		dots: false,
-		autoplay: false,
-		arrows: true,
-		slidesToScroll: 1,
-		prevArrow: '<i class="fas left icon fa-arrow-left"></i>',
-		nextArrow: '<i class="fas right icon fa-arrow-right"></i>',
-		gap: '24px',
-		responsive: [{
-				breakpoint: 992,
-				settings: {
-					slidesToShow: 1,
-				},
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 1,
-					arrows: false,
-				},
-			},
-			{
-				breakpoint: 576,
-				settings: {
-					slidesToShow: 1,
-					arrows: false,
-				},
-			},
-		],
-	});
-
-
-	// all team area slider js
-	$(".all-team-area-slider").slick({
-		infinite: true,
-		speed: 500,
-		slidesToShow: 1,
-		dots: true,
-		autoplay: false,
-		arrows: false,
-		slidesToScroll: 1,
-		prevArrow: '<i class="fas left icon fa-arrow-left"></i>',
-		nextArrow: '<i class="fas right icon fa-arrow-right"></i>',
-		gap: '24px',
-		responsive: [{
-				breakpoint: 992,
-				settings: {
-					slidesToShow: 1,
-				},
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 1,
-				},
-			},
-		],
-	});
-
-
-	// About testimonial slider js
-	$(".about-testimonial-slider").slick({
-		infinite: true,
-		speed: 500,
-		slidesToShow: 1,
-		dots: false,
-		autoplay: false,
-		arrows: true,
-		slidesToScroll: 1,
-		prevArrow: '<i class="fas left icon fa-arrow-left"></i>',
-		nextArrow: '<i class="fas right icon fa-arrow-right"></i>',
-		gap: '24px',
-		responsive: [{
-				breakpoint: 992,
-				settings: {
-					slidesToShow: 1,
-				},
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 1,
-				},
-			},
-		],
-	});
+	// // Reinitialize slider on window resize
+	// $(window).on('resize', function() {
+	// 	initializeSlider();
+	// });
 
 	/* Data Background js */
 
 	$("[data-background]").each(function () {
 		$(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
+		// $(this).css("")
 	})
 
 
